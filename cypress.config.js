57358826b61281c64a76f0818835ec72a4a62c69
Giedrius Tusinskis <giedriustusinskis@gmail.com>
2024-12-20 17:04:05 +0200

    setting up allure reporter

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,16 @@
+const { allureCypress } = require("allure-cypress/reporter");
 const { defineConfig } = require("cypress");
 require('dotenv').config();
 
 module.exports = defineConfig({
-  env: {...process.env},
+  env: { ...process.env },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      allureCypress(on, config, {
+        resultsDir: "allure-results",
+      });
+      return config;
     },
     specPattern: 'cypress/e2e/*.tests.{js, jsx, ts, tsx, feature, features, spec}'
   },
--- cypress/e2e/login.tests.js
+++ cypress/e2e/login.tests.js
@@ -1,12 +1,25 @@
+import * as allure from "allure-js-commons";
 import UserProfilePage from "../pageObjects/userProfilePage";
 
 const emails = require("../fixtures/credentials.json");
 
 describe('Juice shop logins', () => {
     it('Login with bender account. SQL injection', () => {
+        allure.severity("critical");
         const userProfilePage = new UserProfilePage();
         
         userProfilePage.login(emails.sql_injection + "'--");
+        userProfilePage.navigateToUserProfilePage();
+        userProfilePage.assertUserProfileIsOpened(emails.sql_injection);
+    });
+
+    
+    it('Login with bender account. SQL injection. Failing test to check if screenshot is displayed correctly', () => {
+        allure.severity("critical");
+        const userProfilePage = new UserProfilePage();
+        
+        userProfilePage.login(emails.sql_injection + "'--");
+        cy.get('#fail here').click();
         userProfilePage.navigateToUserProfilePage();
         userProfilePage.assertUserProfileIsOpened(emails.sql_injection);
     })
